3c7a7389d17d719799672a50de6b456bdc18a4dd
Luca180405 <daguannoluca16@gmail.com>
2021-09-27 17:07:30 +0200

    🎉 initial commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { Router } from "@roxi/routify";
+  import { routes } from "../.routify/routes";
+</script>
+
+<Router {routes} />
+
+<style global>
+  @import "./global.css";
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;  
+</style>
