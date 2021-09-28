296e0d2585f11704b6963d9317afafd8e54dd7b3
Luca <65894806+Luca180405@users.noreply.github.com>
2021-09-28 21:36:34 +0200

    Add files via upload

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -5,5 +5,18 @@
     metatags.description = 'Description coming soon...'
 </script>
 
-<h1 class="py-10 text-2xl text-center text-yellow-500">Fluffy Limonadenfabrik</h1>
-<p class="text-center text-blue-500">A Test</p>
+<img class="center" src="./src/assets/fonts/img/orangejuice.png" alt="Fluffy Logo">
+<h1 class="py-10 text-2xl font-bold text-center text-yellow-500">Fluffy Limonadenfabrik</h1>
+<p class="pb-10 font-bold text-center text-blue-500">A Test</p>
+
+<h2 class="text-center">Gewinnspiel</h2>
+<p class="text-center">Machen Sie mit und Sie erhalten die Chance auf wertvolle Preise: <br> <br> </p>
+<ul class="text-center">
+    <li>1. Preis: Eine Reise in den Harz</li>
+    <li>2. Preis: Ein Jahr ein Monatsabo an Limonade</li>
+    <li>3. Preis: Ein Warengutschein über 50 Euro für alle Produkte der Fluffy-GmbH</li>
+    <br> <br>
+</ul>
+<p class="text-center">Teilnehmen kann jeder der mindestens 18 Jahre alt ist. Mitmachen kann man noch bis zum 31.10.2007. Die Preisfrage lautet: Wieviele Mitarbeiter hat die Fluffy GmbH zur Zeit?</p>
+<br>
+<p class="text-center"><b>Viel Erfolg!</b></p>
