f326aa42cd53aaa0fca2922303994d199037e3f6
Luca <65894806+Luca180405@users.noreply.github.com>
2021-09-29 21:00:31 +0200

    Add files via upload

--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -5,9 +5,8 @@
     metatags.description = 'Description coming soon...'
 </script>
 
-<img class="center" src="./src/assets/fonts/img/orangejuice.png" alt="Fluffy Logo">
+<img class="w-1/5 h-1/5 center" src="./img/orangejuice.png" alt="Fluffy Logo">
 <h1 class="py-10 text-2xl font-bold text-center text-yellow-500">Fluffy Limonadenfabrik</h1>
-<p class="pb-10 font-bold text-center text-blue-500">A Test</p>
 
 <h2 class="text-center">Gewinnspiel</h2>
 <p class="text-center">Machen Sie mit und Sie erhalten die Chance auf wertvolle Preise: <br> <br> </p>
@@ -19,4 +18,4 @@
 </ul>
 <p class="text-center">Teilnehmen kann jeder der mindestens 18 Jahre alt ist. Mitmachen kann man noch bis zum 31.10.2007. Die Preisfrage lautet: Wieviele Mitarbeiter hat die Fluffy GmbH zur Zeit?</p>
 <br>
-<p class="text-center"><b>Viel Erfolg!</b></p>
+<p class="text-center pb-9"><b>Viel Erfolg!</b></p>
