6118af2f353dae6706b20d11042ea30fc3f40d81
Luca180405 <daguannoluca16@gmail.com>
2021-11-04 15:41:55 +0100

    add Handelspartner

--- src/pages/Handelspartner.svelte
+++ src/pages/Handelspartner.svelte
@@ -3,6 +3,74 @@
     
     metatags.title = 'Fluffy - Handelspartner'
     metatags.description = 'Description coming soon...'
+
+    // Set the date we're counting down to
+var countDownDate = new Date("Jan 5, 2022 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in an element with id="demo"
+  document.getElementById("time").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("time").innerHTML = "EXPIRED";
+  }
+}, 1000); 
 </script>
 
-<h1 class="w-full p-64 text-2xl font-bold text-center text-blue-500">Kommt bald... :)</h1>
+<style>
+
+.bgimg {
+  position: relative;
+  bottom: 18rem;
+  color: white;
+  font-family: "Courier New", Courier, monospace;
+  font-size: 25px;
+}
+
+
+.middle {
+  position: absolute;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  text-align: center;
+}
+
+hr {
+  margin: auto;
+  width: 40%;
+} 
+
+.photo {
+  height: auto;
+  width: 100%;
+  background-position: center;
+  background-size: cover;
+  position: relative;
+}
+</style>
+
+<img src="public/img/baustelle.jpg" alt="Baustelle" class="photo"/>
+<div class="bgimg">
+    <div class="middle">
+      <h1>COMING SOON</h1>
+      <hr>
+      <p id="time"></p>
+    </div>
+  </div> 
--- src/pages/quellen.svelte
+++ src/pages/quellen.svelte
@@ -1,3 +1,6 @@
 https://pngtree.com/free-png-vectors/orange-juice-vector
 https://images.unsplash.com/photo-1526676317768-d9b14f15615a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80
 https://images.unsplash.com/photo-1625685959735-77460dc42f06?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=735&q=80
+https://www.hogapage.de/wp-content/uploads/sites/16/2017/11/Fotolia_128426338_Subscription_Monthly_M__1510559082_80.153.210.253-1-900x558.jpg
+https://images.unsplash.com/photo-1632681772668-37521c332685?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1333&q=80
+https://handelsblattgroup.com/wp-content/uploads/2018/03/buehne_unternehmen.jpg
